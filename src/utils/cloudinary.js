88f1bab5c4269e7654b6867dc0441a8f353bf056
troomtroom <pant.tarunpant@gmail.com>
2026-06-06 20:34:48 +0530

    add multer middleware and cloudinary util for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+import { ApiError } from "./ApiError.js" 
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if (!localFilePath){
+            throw new ApiError(400, "File path is missing")
+        }
+
+        //  upload the file on cloudinary
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        // file has been uploaded succesfully
+        console.log("File is uploaded on Cloudinary ",
+            response.url);
+        return response
+    } catch (error) {
+
+        fs.unlinkSync(localFilePath) 
+        // removes the locally saved temp file as the upload operation failed
+        throw new ApiError(
+            500,
+            error.message || "Failed to upload file on Cloudinary"
+        )
+    }
+}
+
+
+
+
+
+export {uploadOnCloudinary}
