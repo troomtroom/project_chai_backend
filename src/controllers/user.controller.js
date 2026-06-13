84fac8dabbfbe39fe342ad96b0be4093f16b1621
troomtroom <pant.tarunpant@gmail.com>
2026-06-13 23:48:56 +0530

    add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js"
 import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { response } from "express";
-import { trusted } from "mongoose";
+import mongoose, { trusted } from "mongoose";
 import jwt from "jsonwebtoken";
 
 const registerUser = asyncHandler( async (req, res) => {
@@ -596,7 +596,61 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
         .json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
 })
 
+const getWatchHistory = asyncHandler(async(req,res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory" ,
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
+    if (!user || user.length === 0) {
+        throw new ApiError(404, "User not found")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully")
+        )
+})
 
 
 export { 
@@ -609,5 +663,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
  }
